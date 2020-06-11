0068a1352c669c7a3cd037f51f13cb9f721366b7
Yashin <Yashin@YASHIN-PC>
2020-06-11 16:20:29 -0600

    Versión #2

---
+++ numerosPares_Yasin/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numerosPares_Yasin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entrada = "";
+            int numero;
+
+            Console.WriteLine("Digite un numero por favor");
+            Console.WriteLine();
+            entrada = Console.ReadLine();
+            numero = Convert.ToInt32(entrada);
+
+            if ((numero % 2) == 0)
+            {
+                Console.WriteLine("El número es par");
+            }
+            else 
+            {
+                Console.WriteLine("El numero es impar");
+            }
+            Console.ReadLine();
+        }
+    }
+}
